9a68af5118592e386737e7fc7489c243acdc9805
Desmon S <desmon.stanford@gmail.com>
2018-03-26 23:42:11 -0400

    revisions

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,132 @@
 var answerArray = [
-  {
-    friendName: "Ahmed",
-    friendPhoto: "ahmed@example.com",
-    q1Answer: "question Answer",
-    q2Answer: "question Answer",
-    q3Answer: "question Answer",
-    q4Answer: "question Answer",
-    q5Answer: "question Answer",
-    q6Answer: "question Answer",
-    q7Answer: "question Answer",
-    q8Answer: "question Answer",
-    q9Answer: "question Answer",
-    q10Answer: "question Answer"
-  }
+	{
+		name: 'Spongebob Squarepants',
+		photo: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.ebayimg.com%2Fimages%2Fg%2F6GAAAOSwRgJXjlHR%2Fs-l300.jpg&imgrefurl=https%3A%2F%2Fwww.ebay.co.uk%2Fitm%2FNickelodeon-Spongebob-Squarepants-Bath-Yellow-Sponge-Children-Kids-Bath-Gift-%2F371687701045&docid=S99IkzsjNq0PwM&tbnid=krLlpEXB3Dvv7M%3A&vet=10ahUKEwjIu5H2nIvaAhUHH6wKHR_vDlAQMwjeASgGMAY..i&w=300&h=281&bih=803&biw=1440&q=spongebob%20squarepants&ved=0ahUKEwjIu5H2nIvaAhUHH6wKHR_vDlAQMwjeASgGMAY&iact=mrc&uact=8',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Patrick',
+		photo: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fpixel.nymag.com%2Fimgs%2Fdaily%2Fselectall%2F2018%2F03%2F05%2F05-patrick.w710.h473.jpg&imgrefurl=http%3A%2F%2Fnymag.com%2Fselectall%2F2018%2F03%2Fevil-patrick-meme-spongebob-tweets.html&docid=WBtOCotm8MJstM&tbnid=Hcyocbrd2GDWAM%3A&vet=10ahUKEwiJjp-VnYvaAhURXa0KHd9tCjQQMwg1KAIwAg..i&w=710&h=473&bih=803&biw=1440&q=patrick%20star&ved=0ahUKEwiJjp-VnYvaAhURXa0KHd9tCjQQMwg1KAIwAg&iact=mrc&uact=8',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Sandy',
+		photo: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fvignette.wikia.nocookie.net%2Fspongebobfanon%2Fimages%2F8%2F86%2FSandy_Spongepocalypse.PNG%2Frevision%2Flatest%3Fcb%3D20170930145828&imgrefurl=http%3A%2F%2Fspongebobfanon.wikia.com%2Fwiki%2FSandy_Cheeks_(Spongepocalypse)&docid=zJFZ-dDD-gS-rM&tbnid=t4FNl5S5GaB2DM%3A&vet=10ahUKEwiOkLihnYvaAhVDMqwKHTBHCOkQMwiYAigHMAc..i&w=1070&h=800&bih=803&biw=1440&q=sandy%20cheeks&ved=0ahUKEwiOkLihnYvaAhVDMqwKHTBHCOkQMwiYAigHMAc&iact=mrc&uact=8',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Mr. Crabs',
+		photo: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fvignette.wikia.nocookie.net%2Fspongebob%2Fimages%2F8%2F8b%2FMr._Krabs_with_a_Mustache.png%2Frevision%2Flatest%3Fcb%3D20161023162913&imgrefurl=http%3A%2F%2Fspongebob.wikia.com%2Fwiki%2FFile%3AMr._Krabs_with_a_Mustache.png&docid=036egId9h99MXM&tbnid=O_g8VtWOPg7tLM%3A&vet=10ahUKEwiH9Z3TnYvaAhVQZKwKHXddCsYQMwjvASgGMAY..i&w=432&h=597&bih=803&biw=1440&q=mr%20crabs&ved=0ahUKEwiH9Z3TnYvaAhVQZKwKHXddCsYQMwjvASgGMAY&iact=mrc&uact=8',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Squidward',
+		photo: 'https://www.google.com/imgres?imgurl=http%3A%2F%2Fi0.kym-cdn.com%2Fentries%2Ficons%2Foriginal%2F000%2F024%2F790%2Fsquidward.jpg&imgrefurl=http%3A%2F%2Fknowyourmeme.com%2Fmemes%2Fsquidward-tentacles&docid=suLHQs8XS4jwPM&tbnid=6Poyu1mfwnpApM%3A&vet=10ahUKEwjim7egnovaAhVOmK0KHUuhAZsQMwj6ASgCMAI..i&w=800&h=450&bih=803&biw=1440&q=squidward&ved=0ahUKEwjim7egnovaAhVOmK0KHUuhAZsQMwj6ASgCMAI&iact=mrc&uact=8',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Mrs. Piggy',
+		photo: 'http://img.mindbodygreen.com/image/upload/c_limit,w_738,f_auto/ftr/Miss-Piggy-large-tonya-312.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Beyonce Knowles',
+		photo: 'http://www.billboard.com/files/styles/article_main_image/public/media/beyonce-feb-2014-billboard-650.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Elmo',
+		photo: 'http://1rjm5qg4v8732kpmn44ya6bj-wpengine.netdna-ssl.com/wp-content/uploads/2012/11/elmo-6601.png',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
 ];
 
 // Note how we export the array. This makes it accessible to other files using require.
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,32 +4,48 @@
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 
-var answerData = require("../data/friends");
-
-
+var friendList = require("../data/friends");
 
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
 module.exports = function(app) {
-
   app.get("/api/survey", function(req, res) {
     res.json(answerData);
   });
 
+  // Add new friend entry
+  app.post("/api/friends", function(req, res) {
+    // Captures new survey data
+    var userSurvey = req.body;
+    var userResponses = userSurvey.scores;
 
+    // Variables to hold best match
+    var friendName = "";
+    var friendImage = "";
+    var totalDifference = 10000;
 
-  app.post("/api/survey", function(req, res) {
-  
- if (questionsAnswered) {
+    // loop through all friends
+    for (var i = 0; i < friendList.length; i++) {
+      // find the difference of each question
+      var diff = 0;
+      for (var j = 0; j < userResponses.length; j++) {
+        diff += Math.abs(friendList[i].scores[j] - userResponses[j]);
+      }
 
- } else {
-     alert("You have not answered all the questions");
- }
+      // If lowest difference, record the friend match
+      if (diff < totalDifference) {
+        totalDifference = diff;
+        friendName = friendList[i].name;
+        friendImage = friendList[i].photo;
+      }
+    }
+
+    // Add new user
+    friendList.push(userSurvey);
+
+    // Send appropriate response
+    res.json({ status: "OK", friendName: friendName, friendImage: friendImage });
   });
-
-
-    console.log(answerData);
-
 };
